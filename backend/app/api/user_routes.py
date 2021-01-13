07604dcf545eb54f4d205123ec1c8ab9d57a81fe
Elijah <ehubb15@gmail.com>
2021-01-13 14:56:38 -0500

    fixed a typo

--- backend/app/api/user_routes.py
+++ backend/app/api/user_routes.py
@@ -48,7 +48,7 @@ def edit_user():
         db.session.commit()
         return "Job Successfully Updated"
 
-    return make_response("Edit Info Required", 401, {"WWW-Authenticate": "Basic realm='Edit Info Required'"})
+    return make_response("Edit Info Required", 404, {"WWW-Authenticate": "Basic realm='Edit Info Required'"})
 
 
 @user_routes.route("/edit/password", methods=["PUT"])
