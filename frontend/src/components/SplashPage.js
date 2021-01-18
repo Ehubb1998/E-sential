00ad8f8cdc4475d9a2ef5786fea3a01dbe65ffde
Elijah <ehubb15@gmail.com>
2021-01-18 02:27:07 -0500

    started on splash page

--- frontend/src/components/SplashPage.js
+++ frontend/src/components/SplashPage.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import splashPageImg1 from "../splashPage-1.png";
 
 const SplashPage = () => {
     return (
-        <h1>Splash Page</h1>
+        <>
+        <div className="splashPage__backgroundDiv">
+            <div className="splashPage__mainDiv">
+                <div className="splashPage__divContainer-image">
+                    <img src={splashPageImg1} className="splashPage__img1" alt="learn" />
+                </div>
+            </div>
+        </div>
+        </>
     )
 }
 
