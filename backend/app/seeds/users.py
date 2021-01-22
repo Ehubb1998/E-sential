d480c026c5ef6ed959a71f9357e6b2072c81b771
Elijah <ehubb15@gmail.com>
2021-01-22 17:31:02 -0500

    bug fixes

--- backend/app/seeds/users.py
+++ backend/app/seeds/users.py
@@ -7,7 +7,7 @@ from app.models import db, User
 def seed_users():
 
     demo = User(firstName="Elijah", lastName="Hubbard", email='demo@demo.com', primaryBank="Bank Of America", job="Disney +",
-                password="pbkdf2:sha256:150000$z5zPxLd4$7f44d32ca52b1008d58ba40ed68ffab2bcce0773a25f037d2d0c3214bba69244")
+                password="Windows8.1")
 
     db.session.add(demo)
     db.session.commit()
