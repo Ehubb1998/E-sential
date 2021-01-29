3f3eb8b3d610910131af91c5656e70faa4fd2f73
Elijah <ehubb15@gmail.com>
2021-01-29 04:42:46 -0500

    everything is presentable now

--- frontend/src/components/menu/StockMarket.js
+++ frontend/src/components/menu/StockMarket.js
@@ -1,9 +1,40 @@
+import React from "react";
+import Particles from 'react-particles-js';
+
+
 const StockMarket = () => {
     return (
         <>
         <div className="menuSelection__backgroundDiv">
             <div className="menuSelection__mainDiv">
-                <div className="inner__mainDiv">Stock Market</div>
+                <div style={{ position: "relative" }} className="inner__mainDiv">
+                    <Particles
+                        className='particles'
+                        height="520px"
+                        params={{
+                            particles: {
+                                color: {
+                                    value: 'rgb(0, 200, 5)',
+                                },
+                                number: {
+                                    value: 50,
+                                },
+                                size: {
+                                    value: 4,
+                                },
+                            },
+                            interactivity: {
+                                events: {
+                                    onhover: {
+                                        enable: true,
+                                        mode: 'repulse',
+                                    },
+                                },
+                            },
+                        }}
+                    />
+                    <div className="buyStockDiv">Buy Stocks to see portfolio</div>
+                </div>
             </div>
         </div>
         </>
