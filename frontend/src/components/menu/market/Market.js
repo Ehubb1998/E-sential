ad997f39944943d5ce49fc45b33b32fde67d0be0
Elijah <ehubb15@gmail.com>
2021-02-15 15:22:20 -0500

    still working on market component square layout

--- frontend/src/components/menu/market/Market.js
+++ frontend/src/components/menu/market/Market.js
@@ -1,9 +1,27 @@
 import React from "react";
 
 const Market = () => {
+    const featuredCount = 9;
+    const featuredStocks = ["SNAP", "AAPL", "TWTR", "TSLA", "NFLX", "FB", "MSFT", "DIS", "GPRO", "SBUX", "GME", "UBER"];
     return (
         <>
-        <div style={{ fontSize: "18px" }}>Market</div>
+        <div className="stockContent__div">
+            <div className="portfolio__totalValue-container">
+                <div className="totalValue__div">
+                    <div className="totalValue">Featured Stocks</div>
+                </div>
+            </div>
+            <div className="totalValue__bottomBorder"></div>
+            <div style={{ height: "200%" }} className="stockChart">
+                <div className="featuredStocks__container">
+                    {featuredStocks.map(() => (
+                        <div className="featuredStocks__div"></div>
+                    ))}
+                </div>
+                {/* <div style={{ backgroundColor: "grey", height: "5vh" }}></div> */}
+                {/* <div style={{ height: "3%", backgroundColor: "white" }}></div> */}
+            </div>
+        </div>
         </>
     )
 }
